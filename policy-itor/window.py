15e68035a957feaab002cc2b6d23b59d3b3c4020
jooken <jooken@eodiem.com>
2023-09-26 00:27:11 +0900

    창 설정해보기

--- policy-itor/window.py
+++ policy-itor/window.py
@@ -2,4 +2,8 @@ import tkinter as tk
 
 window = tk.Tk() # 기본 윈도우 생성
 
+window.title("Hello")
+window.geometry("640x400+100+100") #창의 너비x높이+x좌표+y좌표 지정
+window.resizable(False, False) #창의 상하, 좌우 조절 지정
+
 window.mainloop()
