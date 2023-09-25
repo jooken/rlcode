ca19bbad5cd6a84a5acbd749948fbae9f0545b78
jooken <jooken@eodiem.com>
2023-09-26 00:54:58 +0900

    레이블 스타일 지정하기

--- policy-itor/window.py
+++ policy-itor/window.py
@@ -6,7 +6,8 @@ window.title("Hello")
 window.geometry("640x400+100+100") #창의 너비x높이+x좌표+y좌표 지정
 window.resizable(False, False) #창의 상하, 좌우 조절 지정
 
-label=tkinter.Label(window, text="Hello") # 레이블 생성
-label.pack() #위젯 배치
+# 레이블의 크기, 글자색, 테두리 스타일 지정하기
+label=tkinter.Label(window, text="Python", width=10, height=5, fg="red", relief="sunken")
+label.pack()
 
 window.mainloop()
